828b91a7cffa8264e389ae404380ec183c86f933
benjam-26 <benjam26@gmail.com>
2023-06-17 10:41:16 -0400

    add login page and styling

--- client/components/Login/Login.js
+++ client/components/Login/Login.js
@@ -1,10 +1,52 @@
-import React from "react";
-import styles from "./Login.scss"
+import React, { useState } from 'react';
+//import PropTypes from 'prop-types';
 
-export const Login = () => {
-  return (
-    <div>
-      <h1>This is the login page.</h1>
+import './Login.scss';
+
+// async function loginUser(credentials) {
+//     return fetch('http://localhost:8080/login', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(credentials)
+//     })
+//       .then(data => data.json())
+//    }
+
+export function Login() {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+//   const handleSubmit = async e => {
+//     e.preventDefault();
+//     const token = await loginUser({
+//       username,
+//       password
+//     });
+//     setToken(token);
+  //}
+
+  return(
+    <div className="login-wrapper">
+      <h1>Welcome! Please Log In</h1>
+      <form >
+        <label>
+          <p>Username</p>
+          <input type="text" onChange={e => setUserName(e.target.value)}/>
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" onChange={e => setPassword(e.target.value)}/>
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   )
-};
+}
+
+// Login.propTypes = {
+//     setToken: PropTypes.func.isRequired
+//   }
