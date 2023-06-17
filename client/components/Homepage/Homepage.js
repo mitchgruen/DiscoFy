c968f03c0045947ff4806a6464014557c173a54e
joey-cheng-codes <j.cheng93@gmail.com>
2023-06-17 10:40:08 -0400

    add bootstrap

--- client/components/Homepage/Homepage.js
+++ client/components/Homepage/Homepage.js
@@ -1,8 +1,22 @@
 import React from "react";
 import styles from './Homepage.js'
+import Button from 'react-bootstrap/Button';
 
 import { Link } from "react-router-dom";
 export const Homepage = () => {
+  // need to be able to make a get request to chatgbt
+  // need to create a button handler
+  const buttonHandler = () => {
+    useEffect(() => {
+      fetch('URL')
+        .then(data => { return data.json() })
+        .then((data) => {
+
+        }).catch((err) => {
+          console.log(err, 'error on the fetch get request')
+        })
+    }, [])
+  }
   return (
     <div>
       <h1>testing from the home page</h1>
