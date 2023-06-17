55e50e7b449617486ee2966c142680f3c126353b
benjam-26 <benjam26@gmail.com>
2023-06-17 14:26:59 -0400

    add signin and login styling

--- client/components/Signup/Signup.js
+++ client/components/Signup/Signup.js
@@ -1,8 +1,43 @@
 import React from "react";
+import styles from './Signup.scss'
+import Button from 'react-bootstrap/Button';
+
+
 export const Signup = () => {
-  return (
-    <div>
-      <h1>testing from the signup page</h1>
+  // const [username, setUserName] = useState();
+  // const [password, setPassword] = useState();
+
+//   const handleSubmit = async e => {
+//     e.preventDefault();
+//     const token = await loginUser({
+//       username,
+//       password
+//     });
+//     setToken(token);
+  //}
+
+  return(
+    <div className="login-wrapper">
+      <h1>Please Signup </h1>
+      <form >
+        <label>
+          <p>Username</p>
+          <input type="text" placeholder="Enter ussername" /> 
+          {/* onChange={e => setUserName(e.target.value)} */}
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" placeholder="Enter password" />
+          {/* onChange={e => setPassword(e.target.value)} */}
+        </label>
+        <div>
+        <Button type="submit" variant="primary">Submit</Button>
+        </div>
+      </form>
     </div>
   )
-};
+}
+
+// Login.propTypes = {
+//     setToken: PropTypes.func.isRequired
+//   }
