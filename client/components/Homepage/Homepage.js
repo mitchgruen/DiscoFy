f9831ce0f627589bb0869656b7c7dd5cb3a596c0
joey-cheng-codes <j.cheng93@gmail.com>
2023-06-17 08:55:57 -0400

    fixed webpack error

--- client/components/Homepage/Homepage.js
+++ client/components/Homepage/Homepage.js
@@ -6,7 +6,11 @@ export const Homepage = () => {
   return (
     <div>
       <h1>testing from the home page</h1>
-      <Link to="/idea">  <button>Generate Idea</button></Link>
+      <Link to="/idea">  <button>Generate Event Ideas</button></Link>
+      <form>
+        <input type="text" className="input-box" placeholder="Your Idea"></input>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
