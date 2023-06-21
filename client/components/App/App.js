fc2cf2a59a75209f0d82700db93ffaf77b53658e
Isaac Lee <isaaclee@Isaacs-MacBook-Pro.local>
2023-06-21 10:58:01 -0400

    finish Authentication

--- client/components/App/App.js
+++ client/components/App/App.js
@@ -1,13 +1,12 @@
-
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Login } from '../Login/Login';
-import { Homepage } from '../Homepage/Homepage'
-import { Idea } from '../Idea/Idea'
-import { Signup } from '../Signup/Signup'
-import { NotFound } from '../NotFound/NotFound'
+import { Login } from "../Login/Login";
+import { Homepage } from "../Homepage/Homepage";
+import { Idea } from "../Idea/Idea";
+import { Signup } from "../Signup/Signup";
+import { NotFound } from "../NotFound/NotFound";
 import { Link } from "react-router-dom";
-import './App.scss';
+import "./App.scss";
 export function App() {
   return (
     <>
@@ -25,12 +24,11 @@ export function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Homepage />} />
-        <Route path="/signup" element={< Signup />} />
-        <Route path="/idea" element={< Idea />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/idea" element={<Idea />} />
         {/* this route is for pages that doesn't exist.  */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
-  )
+  );
 }
-
