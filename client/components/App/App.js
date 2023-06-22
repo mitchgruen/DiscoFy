8c88c164faf9d52fd0658398858bb513123b8c45
Jordan Lopez <jordanlopez992@gmail.com>
2023-06-22 14:19:31 -0400

    Homepage style

--- client/components/App/App.js
+++ client/components/App/App.js
@@ -12,22 +12,10 @@ export function App() {
   const [user, setUser] = useState({});
   return (
     <>
-      <ul>
-        <li>
-          <Link to="/homepage">Home</Link>
-        </li>
-        <li>
-          <Link to="/login">Login Page</Link>
-        </li>
-        <li>
-          <Link to="/chat">Chat</Link>
-        </li>
-      </ul>
       <Routes>
         <Route index element={<Login setUser={setUser} />} />
         <Route path="/home" element={<Homepage user={user} />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
         <Route path="/chat" element={<Chat />} />
         {/* this route is for pages that doesn't exist.  */}
         <Route path="*" element={<NotFound />} />
