e816f706b1199fcde5fb1dd42830fbbd190506c3
Mitch Gruen <mitchgruen.dev@gmail.com>
2024-05-14 12:08:18 -0400

    Delete API directory

--- API/model/apiModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose'),
-Schema = mongoose.Schema;
-
-const ApiSchema = new Schema({
-  messages: {type: String}
-})
-
-module.exports = mongoose.model('ApiSchema', ApiSchema);
